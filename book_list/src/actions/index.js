3943437209fee209c52807e449d320bfa08b8430
titus jin <titusjin@gmail.com>
2017-01-30 12:41:22 +0800

    adding some class notes for better understanding.

--- book_list/src/actions/index.js
+++ book_list/src/actions/index.js
@@ -1,6 +1,6 @@
 function selectBook(book) {
   // selectBook is an ActionCreator, it needs to return an action,
-  // an object with a type property.
+  // an object with property called : 'type'.
   return {
     type: 'BOOK_SELECTED',
     payload: book
--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -3,6 +3,11 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
+    // first we can use like : state.push(action.payload.data)
+    // to make the state updated with new fetching weather data.
+    // But in Redux : we don't mutate state so better we do is :
+    // state.concate(action.payload.data)
+    // ES6 we can do like below:
     return [ action.payload.data, ...state ];
   }
   return state;
